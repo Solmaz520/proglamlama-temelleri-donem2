229393c61cfbabf84578bb078eba21d3c64d2085
Solmaz520 <yuzuaksolmaz@gmail.com>
2025-03-07 10:38:51 +0300

    Add files via upload

---
+++ 250307/ornek2.py
@@ -0,0 +1,5 @@
+sayilar=[35,26,81,64]
+#listeyi büyükten küçüğe sırala
+sayilar.sort()
+sayilar.reverse()
+print(sayilar)
---
+++ 250307/ornek6.py
@@ -0,0 +1,4 @@
+ondalikli_sayilar=[1.4,6.8]
+sayilar=[35,26,81,64]
+sayilar.extend(ondalikli_sayilar)
+print(sayilar)
